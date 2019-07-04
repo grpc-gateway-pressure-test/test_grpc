6459ed07b30ec2a5bdfba890c7bd10dc69865e5b
NikoKVCS <kvcsgame@hotmail.com>
2019-07-04 10:37:21 +0800

    s

---
+++ gatewaymain.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	gw "test/gw/pb"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var (
+	echoEndpoint = flag.String("echo_endpoint", "localhost:51001", "endpoint of YourService")
+)
+
+func run() error {
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterEchoServiceHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":9090", mux)
+}
+
+func main3() {
+	flag.Parse()
+	defer glog.Flush()
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+
+}
